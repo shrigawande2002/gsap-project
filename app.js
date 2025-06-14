544cb9d34e2eb1cd6ccdbaab68a1a920cb0a8358
Shrihari gawande <gawandesj@rknec.edu>
2025-06-14 22:45:57 +0530

    hero section

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
+const menuButton = document.getElementById("menu");
+const mobileMenu = document.getElementById("mobile-menu");
+const closeBtn = document.getElementById("close");
+
+// Initially hide the menu
+mobileMenu.style.display = "none";
+
+menuButton.addEventListener("click", function () {
+  // Show menu
+  mobileMenu.style.display = "flex";
+
+  // Run GSAP animation
+  gsap.from("#mobile-menu", {
+    y: -200,
+    delay: 0.2,
+    opacity: 0,
+    duration: 0.5,
+    ease: "power3.out"
+  });
+});
+
+closeBtn.addEventListener("click", function () {
+  // Animate and then hide
+  gsap.to("#mobile-menu", {
+    y: -200,
+    opacity: 0,
+    duration: 0.3,
+    ease: "power2.in",
+    onComplete: () => {
+      mobileMenu.style.display = "none";
+    }
+  });
+});
 
 
 gsap.from('#hero ', {
@@ -7,3 +40,14 @@ gsap.from('#hero ', {
     y:500,
     ease: "power4.out"
 })
+
+gsap.from("#hero-text .line", {
+  y: 100,
+  scale: 0,
+  delay: 0.5,
+  opacity: 0,
+  duration: 0.5,
+  stagger: 0.3,
+  rotate: 10
+});
+
