d3abefcc3602b1ceb7511ed16a8ae93ac96dde4e
Shrihari Gawande <gawandesj@rknec.edu>
2025-06-16 02:15:02 +0530

    text section

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const menuButton = document.getElementById("menu");
 const mobileMenu = document.getElementById("mobile-menu");
 const closeBtn = document.getElementById("close");
+var tl = gsap.timeline();
 
 // Initially hide the menu
 mobileMenu.style.display = "none";
-
 menuButton.addEventListener("click", function () {
   // Show menu
   mobileMenu.style.display = "flex";
@@ -51,3 +51,95 @@ gsap.from("#hero-text .line", {
   rotate: 10
 });
 
+const heroImg = document.querySelector("#hero-img");
+const heroImg1 = document.querySelector("#hero-img1");
+const heroImg3 = document.querySelector("#hero-img3");
+
+// Hover in
+heroImg.addEventListener("mouseenter", () => {
+  gsap.to(heroImg, {
+    scale: 2,
+    rotate: -5,
+    duration: 0.5,
+    ease: "power2.out"
+  });
+});
+
+heroImg3.addEventListener("mouseenter", () => {
+  gsap.to(heroImg3, {
+    scale: 2,
+    rotate: -5,
+    duration: 0.5,
+    ease: "power2.out"
+  });
+});
+
+heroImg1.addEventListener("mouseenter", () => {
+  gsap.to(heroImg1, {
+    scale: 2,
+    rotate: 5,
+    duration: 0.5,
+    ease: "power2.out"
+  });
+});
+
+// Hover out (reset to normal)
+heroImg.addEventListener("mouseleave", () => {
+  gsap.to(heroImg, {
+    scale: 1,
+    rotate: 0,
+    duration: 0.5,
+    ease: "power2.inOut"
+  });
+});
+
+heroImg1.addEventListener("mouseleave", () => {
+  gsap.to(heroImg1, {
+    scale: 1,
+    rotate: 0,
+    duration: 0.5,
+    ease: "power2.inOut"
+  });
+});
+
+heroImg3.addEventListener("mouseleave", () => {
+  gsap.to(heroImg3, {
+    scale: 1,
+    rotate: 0,
+    duration: 0.5,
+    ease: "power2.inOut"
+  });
+});
+
+const duration = 0.4;
+const gap = 0.2;
+
+gsap.from("#dot1", {
+  y: -10,
+  opacity: 0.2,
+  duration: duration,
+  repeat: -1,
+  yoyo: true,
+  ease: "power1.inOut",
+  delay: 0
+});
+
+gsap.from("#dot2", {
+  y: -10,
+  opacity: 0.2,
+  duration: duration,
+  repeat: -1,
+  yoyo: true,
+  ease: "power1.inOut",
+  delay: gap
+});
+
+gsap.from("#dot3", {
+  y: -10,
+  opacity: 0.2,
+  duration: duration,
+  repeat: -1,
+  yoyo: true,
+  ease: "power1.inOut",
+  delay: gap * 2
+});
